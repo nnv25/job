4d5f9149a355dcc03ed6e2f9cf16423f6efc8ef8
nnv25 <nnv25@mail.ru>
2025-02-06 20:33:51 +0900

    060225

---
+++ server/controllers/companyController.js
@@ -0,0 +1,113 @@
+import Company from "../models/Company.js";
+import Job from "../models/Job.js";
+import bcrypt from "bcrypt";
+import { v2 as cloudinary } from "cloudinary";
+import generateToken from "../utils/generateToken.js";
+
+//Register a new company
+export const registerCompany = async (req, res) => {
+  const { name, email, password } = req.body;
+  /*const imageFile = req.file;*/
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ success: false, message: "Missing details" });
+  }
+
+  try {
+    const companyExists = await Company.findOne({ email });
+    if (companyExists) {
+      return res.status(400).json({
+        success: false,
+        message: "Company already registered",
+      });
+    }
+
+    // Хешируем пароль
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    // Загружаем изображение на Cloudinary
+    /*const imageUpload = await cloudinary.uploader.upload(imageFile.path);*/
+
+    // Создаем компанию
+    const company = await Company.create({
+      name,
+      email,
+      password: hashPassword,
+      /*image: imageUpload.secure_url,*/
+    });
+
+    res.status(201).json({
+      success: true,
+      company: {
+        _id: company._id,
+        name: company.name,
+        email: company.email,
+        /*image: company.image,*/
+      },
+      token: generateToken(company._id),
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+//Company login
+export const loginCompany = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const company = await Company.findOne({ email });
+    if (bcrypt.compare(password, company.password)) {
+      res.json({
+        success: true,
+        company: {
+          _id: company._id,
+          name: company.name,
+          email: company.email,
+        },
+        token: generateToken(company._id),
+      });
+    } else {
+      res.json({ success: false, message: "Invalid email or password" });
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+//Get company data
+export const getCompanyData = async (req, res) => {};
+
+//Post a new job
+export const postJob = async (req, res) => {
+  const { title, description, location, salary, level, category } = req.body;
+  const companyId = req.company._id;
+  try {
+    const newJob = new Job({
+      title,
+      description,
+      location,
+      salary,
+      companyId,
+      date: Date.now(),
+      level,
+      category,
+    });
+    await newJob.save();
+    res.json({ success: true, newJob });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//Get Company Job Applicants
+export const getCompanyJobApplicants = async (req, res) => {};
+
+//Get Company Posted Jobs
+export const getCompanyPostedJobs = async (req, res) => {};
+
+//Change Job Application Status
+export const ChangeJobApplicationsStatus = async (req, res) => {};
+
+//Change job visibility
+export const changeVisibility = async (req, res) => {};
--- server/server.js
+++ server/server.js
@@ -3,12 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/db.js";
 import { clerkWebhooks } from "./controllers/webhooks.js";
+import companyRoutes from "./routes/companyRoutes.js";
+import connectCloudinary from "./config/cloudinary.js";
+import jobRoutes from "./routes/jobRoutes.js";
 
 //Initialize Express
 const app = express();
 
 //Connect to database
 await connectDB();
+await connectCloudinary();
 
 //Middlewars
 app.use(cors());
@@ -17,6 +21,8 @@ app.use(express.json());
 //Routes
 app.get("/", (req, res) => res.send("API Working"));
 app.post("/webhooks", clerkWebhooks);
+app.use("/api/company", companyRoutes);
+app.use("/api/jobs", jobRoutes);
 
 //Port
 const PORT = process.env.PORT || 5000;
