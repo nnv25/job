e3f183bfadfd0bb101640fb81b60864cfc3fbf81
Пользователь <nnv25@mail.ru>
2025-02-10 14:52:38 +0900

    100225

--- client/src/pages/ManageJobs.jsx
+++ client/src/pages/ManageJobs.jsx
@@ -1,12 +1,36 @@
-import React, { useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { assets, manageJobsData } from '../assets/assets';
 import moment from 'moment'
 import { useNavigate } from 'react-router-dom';
+import {AppContext} from '../context/AppContext'
+import axios from 'axios';
+import {toast} from 'react-toastify'
 
 const ManageJobs = () => {
 
   const navigate = useNavigate()
   const [jobs, setJobs] = useState(false)
+  const {backendUrl, companyToken} = useContext(AppContext)
+
+  //Function to fetch company Job Applications data
+  const fetchCompanyJobs = async () => {
+    try {
+      const {data} = await axios.get(backendUrl+'/api/company/list-jobs',{headers:{token:companyToken}})
+      if(data.success) {
+        setJobs(data.jobsData.reverse()) //????
+      }else {
+        toast.error(data.message)
+      }
+    } catch (error) {
+        toast.error(error.message)
+    }
+  }
+
+  useEffect(()=>{
+    if(companyToken) {
+      fetchCompanyJobs()
+    }
+  },[companyToken])
 
   return (
     <div className='container p-4 max-w-5xl'>
@@ -23,7 +47,7 @@ const ManageJobs = () => {
             </tr>
           </thead>
           <tbody>
-            {manageJobsData.map((job, index)=>(
+            {jobs.map((job, index)=>(
               <tr key={index} className='text-gray-700'>
                 <td className='py-2 px-4 border-b max-sm:hidden'>{index+1}</td>
                 <td className='py-2 px-4 border-b'>{job.title}</td>
--- server/controllers/companyController.js
+++ server/controllers/companyController.js
@@ -3,6 +3,7 @@ import Job from "../models/Job.js";
 import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 import generateToken from "../utils/generateToken.js";
+import JobApplication from "../models/JobApplication.js";
 
 //Register a new company
 export const registerCompany = async (req, res) => {
@@ -118,8 +119,12 @@ export const getCompanyPostedJobs = async (req, res) => {
   try {
     const companyId = req.company._id;
     const jobs = await Job.find({ companyId });
-    //(ToDo) Adding No. of applicants info in data
-    res.json({ success: true, jobsData: jobs });
+    //Adding No. of applicants info in data
+    const jobsData = await Promise.all(jobs.map(async(job)=>{
+      const applicants = await JobApplication.find({jobId: job._id});
+      return {...job.toObject(),applicants:applicants.length}
+    }))
+    res.json({ success: true, jobsData });
   } catch (error) {
     res.json({ success: false, message: error.message });
   }
